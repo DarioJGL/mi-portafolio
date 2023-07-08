4bb10fe2ebba9f9e5c5413e62693288297db6fc1
DarioJGL <dariojgl0211@gmail.com>
2023-07-08 18:21:11 -0500

    advantges ok

--- src/componets/Advantages/Advantages.jsx
+++ src/componets/Advantages/Advantages.jsx
@@ -8,32 +8,32 @@ export function Advantages () {
       <ul className='container-advantages'>
 
         <Advantage
-          Img='Advantage/img1-services.webp'
+          Img='img1-services.webp'
           Title='Brilliant Designs'
           Descripcion='Visualize your dreams with exquisite UI/UX designs made to captivate and engage.'
         />
         <Advantage
-          Img='Advantage/img2-services.webp'
+          Img='img2-services.webp'
           Title='Brilliant Designs'
           Descripcion='Visualize your dreams with exquisite UI/UX designs made to captivate and engage.'
         />
         <Advantage
-          Img='Advantage/img3-services.webp'
+          Img='img3-services.webp'
           Title='Brilliant Designs'
           Descripcion='Visualize your dreams with exquisite UI/UX designs made to captivate and engage.'
         />
         <Advantage
-          Img='Advantage/img4-services.webp'
+          Img='img4-services.webp'
           Title='Brilliant Designs'
           Descripcion='Visualize your dreams with exquisite UI/UX designs made to captivate and engage.'
         />
         <Advantage
-          Img='Advantage/img5-services.webp'
+          Img='img5-services.webp'
           Title='Brilliant Designs'
           Descripcion='Visualize your dreams with exquisite UI/UX designs made to captivate and engage.'
         />
         <Advantage
-          Img='Advantage/img6-services.webp'
+          Img='img6-services.webp'
           Title='Brilliant Designs'
           Descripcion='Visualize your dreams with exquisite UI/UX designs made to captivate and engage.'
         />
